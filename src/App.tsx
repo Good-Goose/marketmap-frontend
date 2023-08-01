b5228e2541d871bd83891bc7d9a2e246b160f449
xWxfFle <factores175@gmail.com>
2023-08-01 13:11:46 +0300

    feat: added font, input n` button 🧇

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 import { Button } from '@components/ui/button'
+import { Input } from '@components/ui/input'
 
 export const App = () => {
   return (
     <>
-      <div className="text-2xl">test</div>
+      <div className="text-2xl rounded-lg">test</div>
       <Button>Тест</Button>
+      <Button size="full">Тест</Button>
+      <Input placeholder="test" />
     </>
   )
 }
--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -4,12 +4,12 @@ import { cva, type VariantProps } from 'class-variance-authority'
 import * as React from 'react'
 
 const buttonVariants = cva(
-  'inline-flex items-center justify-center rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50',
+  'inline-flex items-center justify-center rounded-md text-lg font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50',
   {
     variants: {
       variant: {
         default:
-          'bg-gradient-to-tr from-[#0E63AF] to-[#007BFF] text-primary-foreground hover:from-[#003299] hover:to-[#003299]',
+          'bg-gradient-to-tr from-[#0E63AF] to-[#007BFF] text-primary-foreground hover:from-[#001AFF] hover:to-[#005CFF] active:from-[#003299] active:to-[#003299]',
         destructive:
           'bg-destructive text-destructive-foreground hover:bg-destructive/90',
         outline:
@@ -20,9 +20,9 @@ const buttonVariants = cva(
         link: 'text-primary underline-offset-4 hover:underline',
       },
       size: {
-        default: 'h-10 px-4 py-2',
-        sm: 'h-9 rounded-md px-3',
-        lg: 'h-11 rounded-md px-8',
+        default: 'px-6 py-4',
+        full: 'w-full px-4 py-2',
+        large: 'h-16 px-6 py-4',
         icon: 'h-10 w-10',
       },
     },
